b5b13c42ffa23b92350fb11021a910df7e5ed19f
Ozan Ali Kayacı <ozanalikayaci@gmail.com>
2023-06-22 21:45:39 +0300

    search

---
+++ src/components/Navbar/Search/Search.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import { useState } from "react";
+
+import { Link } from "react-router-dom";
+
+//css
+import "./Search.scss";
+
+function Search() {
+  const [filteredWord, setFilteredWord] = useState("");
+
+  let handleChange = (e) => {
+    setFilteredWord(e.target.value);
+  };
+
+  return (
+    <div className="search-container">
+      <input
+        type="text"
+        name="search"
+        placeholder="Search..."
+        className="search-input"
+        onChange={handleChange}
+      />
+      <Link className="search-btn" to={`/search/${filteredWord}`}>
+        <i className="fas fa-search"></i>
+      </Link>
+    </div>
+  );
+}
+
+export default Search;
