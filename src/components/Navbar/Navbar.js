34b9d29c635d5918098f4b9152e800e8dce6a799
Ozan Ali Kayacı <ozanalikayaci@gmail.com>
2023-06-22 21:45:17 +0300

    navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,34 @@
 import React from "react";
+
+//css
+import "./Navbar.scss";
+
+//component
+import Search from "./Search/Search.js";
+
+//react router
 import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
-      <Link to="/">Home</Link>
-      <Link to="/characters">Character</Link>
-      <Link to="/episodes">Episodes</Link>
-      <Link to="/locations">Locations</Link>
+    <div className="navbar">
+      <div className="navbar-comp">
+        <Link className="navbar-button logo" to="/">
+          Home
+        </Link>
+        <div className="category-buttons">
+          <Link className="navbar-button" to="characters">
+            Characters
+          </Link>
+          <Link className="navbar-button" to="/episodes">
+            Episodes
+          </Link>
+          <Link className="navbar-button" to="/locations">
+            Locations
+          </Link>
+        </div>
+        <Search />
+      </div>
     </div>
   );
 }
